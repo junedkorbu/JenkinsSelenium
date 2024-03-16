15b7692407c4af71f2673ed2f8d45f2b25bdbbb4
junedkorbu <junedkorbu@gmail.com>
2024-03-16 13:15:31 +0530

    Update project

---
+++ src/test/java/Basic/OrganizationTest.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+
+	@Test
+	public void CreateOrganizationTest()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8888/");
+	}
+}
